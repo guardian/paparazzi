43f5ad7dd6eee2c51f11e940e4a99dc5ce5d4a2e
Laura <lawr@lauragonzalez.cc>
2018-12-17 10:30:42 +0000

    use cwd

--- index.js
+++ index.js
@@ -5,34 +5,35 @@ const chalk = require('chalk');
 const { resolve } = require('path');
 const { readFileSync, mkdirSync } = require('fs');
 
-const takeShot = async (path, { prefix, sizes, out }) => {
-	console.log(`started ${path}`);
+const takeShot = async (route, { prefix, sizes, path }) => {
+	console.log(`started ${route}`);
 	const browser = await puppeteer.launch();
 	const page = await browser.newPage();
-	await page.goto(`${prefix}${path}`);
+	await page.goto(`${prefix}${route}`);
 
 	for (let size in sizes) {
 		await page.setViewport(sizes[size]);
 		await page.screenshot({
-			path: `${out}/${size}-${path.replace(/\//gi, '-')}.png`,
+			path: `${path}/${size}-${route.replace(/\//gi, '-')}.png`,
 		});
-		console.log(chalk.blue(`done    ${path} – ${size}`));
+		console.log(chalk.blue(`done    ${route} – ${size}`));
 	}
-	console.log(chalk.green(`done    ${path} – ALL`));
+	console.log(chalk.green(`done    ${route} – ALL`));
 
 	return browser.close();
 };
 
 (async () => {
 	const { prefix, sizes, routes, out } = JSON.parse(
-		readFileSync(resolve(__dirname, '.paparazzirc'), 'utf8')
+		readFileSync(resolve(process.cwd(), '.paparazzirc'), 'utf8')
 	);
-	mkdirSync(resolve(__dirname, out));
+	const path = resolve(process.cwd(), out);
+	mkdirSync(path);
 	if (!prefix || !sizes || !routes) {
 		console.error(
 			chalk.red('Could not find .paparazzirc in the project folder')
 		);
 		process.exit(1);
 	}
-	await Promise.all(routes.map(r => takeShot(r, { prefix, sizes, out })));
+	await Promise.all(routes.map(r => takeShot(r, { prefix, sizes, path })));
 })();
