f2aefdf9b87737cb16c9e283d5e43c5f2902aff0
Laura <lawr@lauragonzalez.cc>
2018-12-17 10:23:48 +0000

    all set

---
+++ index.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+
+const puppeteer = require('puppeteer');
+const chalk = require('chalk');
+const { resolve } = require('path');
+const { readFileSync } = require('fs');
+
+const takeShot = async (path, { prefix, sizes }) => {
+	console.log(`started ${path}`);
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+	await page.goto(`${prefix}${path}`);
+
+	for (let size in sizes) {
+		await page.setViewport(sizes[size]);
+		await page.screenshot({
+			path: `shots/${size}-${path.replace(/\//gi, '-')}.png`,
+		});
+		console.log(chalk.blue(`done    ${path} – ${size}`));
+	}
+	console.log(chalk.green(`done    ${path} – ALL`));
+
+	return browser.close();
+};
+
+(async () => {
+	const { prefix, sizes, routes } = JSON.parse(
+		readFileSync(resolve(__dirname, '.paparazzirc'), 'utf8')
+	);
+	if (!prefix || !sizes || !routes) {
+		console.error(
+			chalk.red('Could not find .paparazzirc in the project folder')
+		);
+		process.exit(1);
+	}
+	await Promise.all(routes.map(r => takeShot(r, { prefix, sizes })));
+})();
